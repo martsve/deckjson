ca04a89e1cbbdc008189ecf550d9d6cd52b7a8f3
Martin Svendsen <martin.svendsen@helsedir.no>
2019-10-28 14:24:58 +0100

    Updates

---
+++ precache-manifest.2f251bb24a5867267562ab1c3efb8ca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703d141e4201e2707c5b092860a2f482",
+    "url": "/deckjson/index.html"
+  },
+  {
+    "revision": "6e157765b9f0fc33bac0",
+    "url": "/deckjson/static/css/main.ca39013f.chunk.css"
+  },
+  {
+    "revision": "80dbb35304a8f965074d",
+    "url": "/deckjson/static/js/2.ca316b54.chunk.js"
+  },
+  {
+    "revision": "6e157765b9f0fc33bac0",
+    "url": "/deckjson/static/js/main.f4e60621.chunk.js"
+  },
+  {
+    "revision": "9410dfee274445be0972",
+    "url": "/deckjson/static/js/runtime-main.e6d58c00.js"
+  }
+]);
