0248b158bff30609d312c6a095d9d3d06404441e
Martin Svendsen <martin.svendsen@helsedir.no>
2019-10-28 14:33:40 +0100

    change router

--- src/Router/App.js
+++ src/Router/App.js
@@ -6,7 +6,7 @@ import { default as CollectionOverview } from '../modules/Collection/Index';
 import NotFound from '../modules/ErrorPages';
 
 import { Provider } from 'react-redux'
-import { Route, BrowserRouter as Router, Switch, NavLink, Redirect  } from 'react-router-dom'
+import { Route, HashRouter as Router, Switch, NavLink, Redirect  } from 'react-router-dom'
 import { store } from '../store';
 
 const IndexPage = () => (<Redirect to="/decks/" />);
@@ -14,7 +14,7 @@ const IndexPage = () => (<Redirect to="/decks/" />);
 function App() {
   return (
     <Provider store={store}>
-      <Router>
+      <Router basename='/'>
         <nav className='sm-flex'>
           <NavLink exact activeClassName='active' to="/"><span className='icon-home medium'></span></NavLink>
           <NavLink activeClassName='active' to="/decks/">Decks</NavLink>
